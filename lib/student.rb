4e629f15ee4aba0b19408278aa72382a690373e0
Jed Peek <jedpeek@yahoo.com>
2018-07-02 18:48:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name:, grade:, id:nil)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
   def save
