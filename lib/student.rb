746f297766e9a3b708f70fd11ca4d13d2bc00d9a
Jed Peek <jedpeek@yahoo.com>
2018-07-02 18:37:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,6 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name:, grade:, id: nil)
-    @name = name
-    @grade = grade
-  end
-
   def attributes
     @name
     @grade
